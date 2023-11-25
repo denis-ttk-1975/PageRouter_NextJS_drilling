46d4c4807b7c05c6ded09e5d19d18acfa54f8ccb
denis-ttk-1975 <denis.ttk@gmail.com>
2023-11-25 21:14:52 +0300

    Htag added

---
+++ rate-app_nextjs_page-router/components/Htag/Htag.tsx
@@ -0,0 +1,10 @@
+import { IHtagProps } from './Htag.props';
+import styles from './Htag.module.css';
+export const Htag = ({ tag, children }: IHtagProps): JSX.Element => {
+    return (
+    <>
+    {tag === 'h1' && <h1 className = { styles.h1 }>{ children }</h1> }
+    {tag === 'h2' && <h2 className = { styles.h2 }>{ children }</h2> }
+    {tag === 'header_title' && <h3 className = { styles.header_title }>{ children }</h3> }
+    </>)
+}
--- rate-app_nextjs_page-router/pages/index.tsx
+++ rate-app_nextjs_page-router/pages/index.tsx
@@ -1,8 +1,12 @@
+import { Htag } from '@/components';
+
 export default function Home(): JSX.Element {
-  const makar = 'Makar!!!!';
   return (
     <>
-      {makar}
+    <Htag tag = 'h1'>Наименование111</Htag>
+    <Htag tag = 'h2'>Наименование222</Htag>
+    <Htag tag = 'header_title'>Наименование header title</Htag>
+
     </>
   );
 }
