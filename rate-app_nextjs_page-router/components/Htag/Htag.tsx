a3a9217bc316a644c412e1300d37de4249e0f6e5
denis-ttk-1975 <denis.ttk@gmail.com>
2024-02-18 21:48:02 +0300

    warning in styles fixed

--- rate-app_nextjs_page-router/components/Htag/Htag.tsx
+++ rate-app_nextjs_page-router/components/Htag/Htag.tsx
@@ -6,5 +6,6 @@ export const Htag = ({ tag, children }: IHtagProps): JSX.Element => {
     {tag === 'h1' && <h1 className = { styles.h1 }>{ children }</h1> }
     {tag === 'h2' && <h2 className = { styles.h2 }>{ children }</h2> }
     {tag === 'header_title' && <h3 className = { styles.header_title }>{ children }</h3> }
-    </>)
-}
+    </>
+    );
+};
