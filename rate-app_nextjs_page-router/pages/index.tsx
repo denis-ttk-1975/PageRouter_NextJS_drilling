831bed3aad3da0026c2ed5ff58fa9346f30e72b5
denis-ttk-1975 <denis.ttk@gmail.com>
2024-02-25 19:55:26 +0300

    naming for page added

--- rate-app_nextjs_page-router/pages/index.tsx
+++ rate-app_nextjs_page-router/pages/index.tsx
@@ -3,6 +3,7 @@ import { Button, Htag } from '@/components';
 export default function Home(): JSX.Element {
   return (
     <>
+    <Htag tag = 'h1'>Page Router - старый</Htag>
     <Htag tag = 'h1'>Наименование111</Htag>
     <Htag tag = 'h2'>Наименование222</Htag>
     <Htag tag = 'header_title'>Наименование header title</Htag>
