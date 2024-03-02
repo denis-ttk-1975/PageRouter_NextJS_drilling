ce5683e1e47b5dd4d995ed4d7c7e860de96108bc
denis-ttk-1975 <denis.ttk@gmail.com>
2024-03-02 11:54:19 +0300

    css-grid for layout done

--- rate-app_nextjs_page-router/layout/Layout.tsx
+++ rate-app_nextjs_page-router/layout/Layout.tsx
@@ -3,22 +3,20 @@ import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { ILayoutProps } from './Layout.props';
 import { FunctionComponent } from 'react';
-// import styles from './Paragraph.module.css';
+import styles from './Layout.module.css';
 // import cn from 'classnames';
 
 
 const Layout = ({ children }: ILayoutProps): JSX.Element => {
      return (
-     <>
-        <Header/>
-        <div style = {{ display: 'flex' }}>
-            <Sidebar/>
-            <div>
+     <div className = { styles.wrapper }>
+        <Header className = { styles.header } />
+            <Sidebar className = { styles.sidebar } />
+            <div className = { styles.body } >
                 {children}
             </div>
-        </div>
-        <Footer/>
-     </>);
+        <Footer className = { styles.footer } />
+     </div>);
 };
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
