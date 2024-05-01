78ba3707b88981eb7861f8612c2b2e750c84ebc7
denis-ttk-1975 <denis.ttk@gmail.com>
2024-05-01 16:08:30 +0300

    first level menu done

--- rate-app_nextjs_page-router/layout/Menu/Menu.tsx
+++ rate-app_nextjs_page-router/layout/Menu/Menu.tsx
@@ -2,14 +2,47 @@ import { useContext } from 'react';
 import styles from './Menu.module.css';
 import cn from 'classnames';
 import { AppContext } from '@/context/app.context';
+import { FirstLevelMenuItem } from '@/interfaces/menu.interface';
 
+import CoursesIcon from './icons/courses.svg';
+import BooksIcon from './icons/books.svg';
+import ProductsIcon from './icons/products.svg';
+import ServicesIcon from './icons/services.svg';
+import { TopLevelCategory } from '@/interfaces/page.interface';
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+  { route: 'courses', name: 'Курсы', icon: CoursesIcon, id: TopLevelCategory.Courses },
+  { route: 'books', name: 'Книги', icon: BooksIcon, id: TopLevelCategory.Books },
+  { route: 'products', name: 'Продукты', icon: ProductsIcon, id: TopLevelCategory.Products },
+  { route: 'services', name: 'Сервисы', icon: ServicesIcon, id: TopLevelCategory.Services },
+];
 export const Menu = (): JSX.Element => {
+  const { menu, firstCategory, setMenu } = useContext(AppContext);
 
-    const {menu, firstCategory, setMenu} = useContext(AppContext);
+  const buildFirstLevel = () => {
+    return (
+      <>
+        {firstLevelMenu.map((firstMenuItem) => (
+          <div key={firstMenuItem.route}>
+            <a href={`/${firstMenuItem.route}`}>
+              <div
+                className={cn(styles.firstLevel, {
+                  [styles.firstLevelActive]: firstMenuItem.id === firstCategory,
+                })}
+              >
+                {firstMenuItem.icon}
+                <span>{firstMenuItem.name}</span>
+              </div>
+            </a>
+          </div>
+        ))}
+      </>
+    );
+  };
 
-    return <div>
-        <ul>
-              {menu.map(item => (<li key={item._id.secondCategory}>{item._id.secondCategory}</li>))}
-        </ul>
-    </div>;
-    };
+  const buildSecondLevel = () => {};
+
+  const buildThirdLevel = () => {};
+
+  return <div className={styles.menu}>{buildFirstLevel()}</div>;
+};
